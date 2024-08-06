a7b522d61f69084b2acaaa38d5dada556245f0ef
JESSIKRU\punki <jessica.a.krueger@gmail.com>
2024-08-06 15:14:36 -0500

    adding meeting locations info

--- capstone/src/main/java/org/jessicakrueger/capstone/database/DAO/MeetingLocationsDAO.java
+++ capstone/src/main/java/org/jessicakrueger/capstone/database/DAO/MeetingLocationsDAO.java
@@ -1,2 +1,10 @@
-package org.jessicakrueger.capstone.database.DAO;public class MeetingLocationsDAO {
+package org.jessicakrueger.capstone.database.DAO;
+
+import org.jessicakrueger.capstone.database.entity.MeetingLocations;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MeetingLocationsDAO extends JpaRepository<MeetingLocations, Long> {
+
+    MeetingLocations findById(Integer Id);
+
 }
--- capstone/src/main/java/org/jessicakrueger/capstone/database/entity/MeetingLocations.java
+++ capstone/src/main/java/org/jessicakrueger/capstone/database/entity/MeetingLocations.java
@@ -1,2 +1,26 @@
-package org.jessicakrueger.capstone.database.entity;public class MeetingLocations {
+package org.jessicakrueger.capstone.database.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Entity
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "meeting_locations")
+public class MeetingLocations {
+
+    @Id // this tells hibernate that this column is the PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // this tells hibernate that the column is autoincremented
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "meeting_location")
+    private String meetingLocation;
+
+    @Column(name = "description")
+    private String description;
 }
