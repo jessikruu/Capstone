d38e7802b668e52e0aa8bbe51adf7865db79f62c
JESSIKRU\punki <jessica.a.krueger@gmail.com>
2024-08-09 14:30:09 -0500

    added find by methods to bookClubDAO w native query

--- capstone/src/main/java/org/jessicakrueger/capstone/database/DAO/BookClubDAO.java
+++ capstone/src/main/java/org/jessicakrueger/capstone/database/DAO/BookClubDAO.java
@@ -4,7 +4,9 @@ import org.jessicakrueger.capstone.database.entity.BookClub;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.awt.print.Book;
 import java.util.List;
+import java.util.Map;
 
 public interface BookClubDAO extends JpaRepository<BookClub, Long> {
 
@@ -12,4 +14,13 @@ public interface BookClubDAO extends JpaRepository<BookClub, Long> {
     List<BookClub> findBySearchTerm(String name);
 
     BookClub findById(Integer id);
+
+    @Query(value = "select bc.created_by from book_clubs bc where bc.created_by = :id;", nativeQuery = true)
+    public List<Map<String, Object>> getCreatedBy(Integer id);
+
+    List<BookClub> findByCreatedBy(Integer id);
+
+    BookClub findByMeetingLocationId(Integer id);
+
 }
+
