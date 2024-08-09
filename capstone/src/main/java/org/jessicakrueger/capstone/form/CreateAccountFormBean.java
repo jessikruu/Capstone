03b7e9ef0d467181960526d1117a557d1186c02d
JESSIKRU\punki <jessica.a.krueger@gmail.com>
2024-08-09 14:32:49 -0500

    updated createaccountformbean to include new variables from user entity

--- capstone/src/main/java/org/jessicakrueger/capstone/form/CreateAccountFormBean.java
+++ capstone/src/main/java/org/jessicakrueger/capstone/form/CreateAccountFormBean.java
@@ -1,11 +1,16 @@
 package org.jessicakrueger.capstone.form;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class CreateAccountFormBean {
 
     @NotEmpty
@@ -13,6 +18,23 @@ public class CreateAccountFormBean {
 
     @NotEmpty
     private String password;
+
+    @AssertTrue(message="sorry, you gotta be human to ride this ride")
+    private boolean human;
+
+    @AssertTrue(message = "SKINWALKER!!!!!!")
+    private boolean sure;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    private Integer zipCode;
+
+    @NotEmpty
+    private String username;
 
 
 }
