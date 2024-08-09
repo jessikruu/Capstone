9491bfa32894ba0087a3519c7776643de480426d
JESSIKRU\punki <jessica.a.krueger@gmail.com>
2024-08-09 14:30:37 -0500

    userDAO created with findBy methods

--- capstone/src/main/java/org/jessicakrueger/capstone/database/DAO/UserDAO.java
+++ capstone/src/main/java/org/jessicakrueger/capstone/database/DAO/UserDAO.java
@@ -2,11 +2,17 @@ package org.jessicakrueger.capstone.database.DAO;
 
 import org.jessicakrueger.capstone.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Map;
 
 public interface UserDAO extends JpaRepository<User, Long> {
 
     User findByEmailIgnoreCase(String email);
 
     User findById(Integer id);
+
+
 
 }
