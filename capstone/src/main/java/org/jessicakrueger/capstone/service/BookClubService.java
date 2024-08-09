fbf588ecd8742c9ec734b54da8e23a9ef2edca04
JESSIKRU\punki <jessica.a.krueger@gmail.com>
2024-08-09 14:34:07 -0500

    added functionality so that the user is automatically assigned to the book club, as well as the genreId and the MeetinglocationId

--- capstone/src/main/java/org/jessicakrueger/capstone/service/BookClubService.java
+++ capstone/src/main/java/org/jessicakrueger/capstone/service/BookClubService.java
@@ -2,14 +2,19 @@ package org.jessicakrueger.capstone.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.jessicakrueger.capstone.database.DAO.BookClubDAO;
+import org.jessicakrueger.capstone.database.DAO.GenresDAO;
 import org.jessicakrueger.capstone.database.DAO.MeetingLocationsDAO;
 import org.jessicakrueger.capstone.database.DAO.UserDAO;
 import org.jessicakrueger.capstone.database.entity.BookClub;
+import org.jessicakrueger.capstone.database.entity.Genres;
 import org.jessicakrueger.capstone.database.entity.MeetingLocations;
 import org.jessicakrueger.capstone.database.entity.User;
 import org.jessicakrueger.capstone.form.CreateBookClubFormBean;
+import org.jessicakrueger.capstone.security.AuthenticatedUserUtilities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Slf4j
 @Component
@@ -24,6 +29,15 @@ public class BookClubService {
     @Autowired
     UserDAO userDAO;
 
+    @Autowired
+    GenresDAO genresDAO;
+
+    private final AuthenticatedUserUtilities authenticatedUserUtilities;
+
+    public BookClubService(AuthenticatedUserUtilities authenticatedUserUtilities) {
+        this.authenticatedUserUtilities = authenticatedUserUtilities;
+    }
+
     public BookClub createBookClub(CreateBookClubFormBean form) {
 
         log.debug(form.toString());
@@ -34,15 +48,31 @@ public class BookClubService {
         }
 
         bookClub.setClubName(form.getClubName());
-        bookClub.setGenre(form.getGenre());
+//        bookClub.setGenre(form.getGenre());
+        bookClub.setDescription(form.getDescription());
+        bookClub.setCreationDate(new Date());
 
-        log.debug(form.getMeetingLocationId().toString());
+//        log.debug(form.getMeetingLocationId().toString());
 
-        MeetingLocations meetingLocations = meetingLocationsDAO.findById(form.getMeetingLocationId());
-        bookClub.setMeetingLocation(meetingLocations);
 
-        User user = userDAO.findById(form.getCreatedBy());
-        bookClub.setCreatedBy(user.getId());
+
+        MeetingLocations meetingLocation = meetingLocationsDAO.findById(form.getMeetingLocationId());
+        bookClub.setMeetingLocation(meetingLocation);
+
+        Genres genres = genresDAO.findById(form.getGenreId());
+        bookClub.setGenre(genres);
+
+        User currentUser = authenticatedUserUtilities.getCurrentUser(); // Implement this method to get the current user
+        if (currentUser != null) {
+            log.debug("Current user ID: " + currentUser.getId());
+
+//            form.setCreatedBy(currentUser.getId());
+            bookClub.setUser(currentUser);
+
+        } else {
+            log.warn("No current user found");
+        }
+
 
 
         bookClub = bookClubDAO.save(bookClub);
