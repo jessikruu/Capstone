e7dce9349b7ebf1c1a179646ada96190fd51dcd9
JESSIKRU\punki <jessica.a.krueger@gmail.com>
2024-08-09 14:32:16 -0500

    fields added to user entity

--- capstone/src/main/java/org/jessicakrueger/capstone/database/entity/User.java
+++ capstone/src/main/java/org/jessicakrueger/capstone/database/entity/User.java
@@ -37,4 +37,13 @@ public class User {
     @Column(name = "username")
     private String username;
 
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "zip_code")
+    private Integer zipCode;
+
 }
