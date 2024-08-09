50305fedcedac7640ba2bdc0ed0a7f096216ac2d
JESSIKRU\punki <jessica.a.krueger@gmail.com>
2024-08-09 14:36:36 -0500

    discussion controller created

---
+++ capstone/src/main/java/org/jessicakrueger/capstone/controller/DiscussionController.java
@@ -0,0 +1,12 @@
+package org.jessicakrueger.capstone.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/discussion")
+public class DiscussionController {
+}
