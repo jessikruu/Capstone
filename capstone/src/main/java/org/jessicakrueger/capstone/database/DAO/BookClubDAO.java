57621ec2876c833bd96a6054d84550f6b8b43ca5
JESSIKRU\punki <jessica.a.krueger@gmail.com>
2024-08-11 21:18:27 -0500

    query changes

--- capstone/src/main/java/org/jessicakrueger/capstone/database/DAO/BookClubDAO.java
+++ capstone/src/main/java/org/jessicakrueger/capstone/database/DAO/BookClubDAO.java
@@ -3,8 +3,10 @@ package org.jessicakrueger.capstone.database.DAO;
 import org.jessicakrueger.capstone.database.entity.BookClub;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.awt.print.Book;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -22,5 +24,10 @@ public interface BookClubDAO extends JpaRepository<BookClub, Long> {
 
     BookClub findByMeetingLocationId(Integer id);
 
+    @Query(value = "SELECT bc.club_name, bc.genre_id, bc.created_by, bc.creation_date, bc.meeting_location_id, bc.description FROM book_clubs bc INNER JOIN club_members cm ON bc.id = cm.club_id WHERE cm.user_id = :id", nativeQuery = true)
+    ArrayList<BookClub> findBookClubDetails(@Param("id") Integer id);
+
+//    @Query("select * from BookClub b, ClubMembers cm where cm.userId = id and b.id = cm.clubId")
+//    List<BookClub> findBookClubsByUserId(Integer id);
 }
 
