7eef01a02f32819eea893520e2b0e5b4011856ea
JESSIKRU\punki <jessica.a.krueger@gmail.com>
2024-08-11 21:18:59 -0500

    beginning process of creating a discussion post

--- capstone/src/main/java/org/jessicakrueger/capstone/database/DAO/DiscussionDAO.java
+++ capstone/src/main/java/org/jessicakrueger/capstone/database/DAO/DiscussionDAO.java
@@ -1,2 +1,12 @@
-package org.jessicakrueger.capstone.database.DAO;public class DiscussionDAO {
+package org.jessicakrueger.capstone.database.DAO;
+
+import org.jessicakrueger.capstone.database.entity.ClubMembers;
+import org.jessicakrueger.capstone.database.entity.Discussions;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiscussionDAO extends JpaRepository<ClubMembers, Long> {
+
+    Discussions findById(Integer Id);
+
+
 }
--- capstone/src/main/java/org/jessicakrueger/capstone/form/CreateDiscussionFormBean.java
+++ capstone/src/main/java/org/jessicakrueger/capstone/form/CreateDiscussionFormBean.java
@@ -1,12 +1,28 @@
 package org.jessicakrueger.capstone.form;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.Date;
 
 @Getter
 @Setter
 @ToString
 public class CreateDiscussionFormBean {
+
+
+    private Integer clubId;
+
+    private Integer discussionCreator;
+
+    private String body;
+
+    private Date creationDate;
+
+    private String title;
 }
